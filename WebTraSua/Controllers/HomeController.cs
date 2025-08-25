2b53e945a13d2b5eb0297f4696b7ca0df7786444
DANH <DANH@DESKTOP-U9E1OSN>
2025-08-26 01:41:58 +0700

    Add project files.

---
+++ WebTraSua/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTraSua.Controllers
+{
+    public class HomeController : Controller
+    {
+  
+           public ActionResult Index()
+        {
+            // Danh sách ảnh, có thể lấy từ DB
+            var banners = new List<string>
+    {
+        Url.Content("~/images/banner1.jpg"),
+        Url.Content("~/images/banner2.jpg"),
+        Url.Content("~/images/banner3.jpg")
+    };
+
+            ViewBag.Banners = banners;
+            return View();
+        }
+
+   
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
